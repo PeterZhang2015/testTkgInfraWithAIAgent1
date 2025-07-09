b0b4f1e7dde423fabb9ed3911394c01f1622c82a
CHONGZHENG ZHANG <zhangchongzheng@gmail.com>
2025-07-09 22:32:40 +1000

    Add vSphere base infrastructure Terraform module

--- infrastructure/terraform/modules/vsphere-base/main.tf
+++ infrastructure/terraform/modules/vsphere-base/main.tf
@@ -1,196 +1,188 @@
-# vSphere Base Module
-# This module creates the foundational vSphere resources for Tanzu Kubernetes Grid
+# vSphere Base Infrastructure Module
+# This module sets up the foundational vSphere infrastructure components
 
-# Input variables
-variable "datacenter_id" {
-  description = "vSphere datacenter ID"
-  type        = string
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "~> 2.0"
+    }
+  }
 }
 
-variable "datastore_id" {
-  description = "vSphere datastore ID"
-  type        = string
-}
-
-variable "cluster_id" {
-  description = "vSphere cluster ID"
-  type        = string
-}
-
-variable "network_id" {
-  description = "vSphere network ID"
-  type        = string
-}
-
-variable "template_id" {
-  description = "VM template ID"
-  type        = string
-}
-
-variable "resource_pool_name" {
-  description = "Name of the resource pool to create"
-  type        = string
-}
-
-variable "vm_folder_name" {
-  description = "Name of the VM folder to create"
-  type        = string
-}
-
-variable "common_tags" {
-  description = "Common tags to apply to all resources"
-  type        = map(string)
-  default     = {}
-}
-
-# Create resource pool for Tanzu clusters
-resource "vsphere_resource_pool" "tanzu_resource_pool" {
-  name                    = var.resource_pool_name
-  parent_resource_pool_id = var.cluster_id
+# Local values for resource naming
+locals {
+  name_prefix = "${var.environment}-tanzu"
   
-  cpu_share_level         = "normal"
-  cpu_limit               = -1
-  cpu_reservation         = 0
-  cpu_expandable          = true
-  
-  memory_share_level      = "normal"
-  memory_limit            = -1
-  memory_reservation      = 0
-  memory_expandable       = true
-  
-  tags = var.common_tags
+  vm_template_configs = {
+    kubernetes = {
+      name         = "${local.name_prefix}-k8s-template"
+      description  = "Kubernetes node template for ${var.environment} environment"
+      cpu_count    = 2
+      memory_mb    = 4096
+      disk_size_gb = 20
+      guest_id     = "ubuntu64Guest"
+    }
+    
+    ubuntu = {
+      name         = "${local.name_prefix}-ubuntu-template"
+      description  = "Ubuntu base template for ${var.environment} environment"
+      cpu_count    = 1
+      memory_mb    = 2048
+      disk_size_gb = 20
+      guest_id     = "ubuntu64Guest"
+    }
+  }
 }
 
-# Create VM folder for Tanzu clusters
-resource "vsphere_folder" "tanzu_vm_folder" {
-  path          = var.vm_folder_name
+# Create VM folder for organizing Tanzu resources
+resource "vsphere_folder" "tanzu_folder" {
+  path          = "${var.environment}-tanzu-infrastructure"
   type          = "vm"
   datacenter_id = var.datacenter_id
   
   tags = var.common_tags
 }
 
+# Create resource pool for Tanzu workloads
+resource "vsphere_resource_pool" "tanzu_resource_pool" {
+  name                    = "${local.name_prefix}-resource-pool"
+  parent_resource_pool_id = var.resource_pool_id
+  
+  # CPU configuration
+  cpu_expandable   = true
+  cpu_limit        = -1
+  cpu_reservation  = 0
+  cpu_share_level  = "normal"
+  
+  # Memory configuration
+  memory_expandable   = true
+  memory_limit        = -1
+  memory_reservation  = 0
+  memory_share_level  = "normal"
+  
+  tags = var.common_tags
+}
+
 # Create custom attributes for Tanzu resources
-resource "vsphere_custom_attribute" "tanzu_cluster_attribute" {
-  name                = "tanzu-cluster"
+resource "vsphere_custom_attribute" "tanzu_cluster_name" {
+  name                = "tanzu-cluster-name"
   managed_object_type = "VirtualMachine"
 }
 
-resource "vsphere_custom_attribute" "tanzu_role_attribute" {
-  name                = "tanzu-role"
+resource "vsphere_custom_attribute" "tanzu_cluster_role" {
+  name                = "tanzu-cluster-role"
   managed_object_type = "VirtualMachine"
 }
 
-resource "vsphere_custom_attribute" "tanzu_environment_attribute" {
+resource "vsphere_custom_attribute" "tanzu_environment" {
   name                = "tanzu-environment"
   managed_object_type = "VirtualMachine"
 }
 
-# Create vSphere tags and categories for Tanzu
-resource "vsphere_tag_category" "tanzu_cluster_category" {
-  name               = "tanzu-cluster"
-  description        = "Tanzu cluster identification"
-  cardinality        = "SINGLE"
-  associable_types   = ["VirtualMachine"]
+# Create VM templates for Kubernetes nodes (placeholder - actual template creation would be done via Packer)
+# This is a data source reference to templates that would be created separately
+data "vsphere_virtual_machine" "kubernetes_template" {
+  count         = var.create_templates ? 1 : 0
+  name          = "kubernetes-${var.kubernetes_version}-template"
+  datacenter_id = var.datacenter_id
 }
 
-resource "vsphere_tag_category" "tanzu_role_category" {
-  name               = "tanzu-role"
-  description        = "Tanzu node role identification"
-  cardinality        = "SINGLE"
-  associable_types   = ["VirtualMachine"]
-}
-
-resource "vsphere_tag_category" "tanzu_environment_category" {
-  name               = "tanzu-environment"
-  description        = "Tanzu environment identification"
-  cardinality        = "SINGLE"
-  associable_types   = ["VirtualMachine"]
-}
-
-# Create tags for management cluster
-resource "vsphere_tag" "management_cluster_tag" {
-  name        = "management"
-  category_id = vsphere_tag_category.tanzu_cluster_category.id
-  description = "Management cluster VMs"
-}
-
-# Create tags for development cluster
-resource "vsphere_tag" "dev_cluster_tag" {
-  name        = "development"
-  category_id = vsphere_tag_category.tanzu_cluster_category.id
-  description = "Development cluster VMs"
-}
-
-# Create tags for production cluster
-resource "vsphere_tag" "prod_cluster_tag" {
-  name        = "production"
-  category_id = vsphere_tag_category.tanzu_cluster_category.id
-  description = "Production cluster VMs"
-}
-
-# Create tags for node roles
-resource "vsphere_tag" "control_plane_tag" {
-  name        = "control-plane"
-  category_id = vsphere_tag_category.tanzu_role_category.id
-  description = "Control plane node VMs"
-}
-
-resource "vsphere_tag" "worker_tag" {
-  name        = "worker"
-  category_id = vsphere_tag_category.tanzu_role_category.id
-  description = "Worker node VMs"
-}
-
-# Create tags for environments
-resource "vsphere_tag" "dev_environment_tag" {
-  name        = "dev"
-  category_id = vsphere_tag_category.tanzu_environment_category.id
-  description = "Development environment"
-}
-
-resource "vsphere_tag" "prod_environment_tag" {
-  name        = "prod"
-  category_id = vsphere_tag_category.tanzu_environment_category.id
-  description = "Production environment"
-}
-
-resource "vsphere_tag" "mgmt_environment_tag" {
-  name        = "mgmt"
-  category_id = vsphere_tag_category.tanzu_environment_category.id
-  description = "Management environment"
-}
-
-# Create DRS VM groups for anti-affinity
+# Create DRS rules for HA deployment
 resource "vsphere_drs_vm_override" "tanzu_drs_override" {
-  compute_cluster_id = var.cluster_id
-  virtual_machine_id = var.template_id
-  
+  count                = var.enable_drs ? 1 : 0
+  compute_cluster_id   = var.cluster_id
+  virtual_machine_id   = data.vsphere_virtual_machine.kubernetes_template[0].id
   drs_enabled          = true
   drs_automation_level = "fullyAutomated"
 }
 
-# Data source to get cluster hosts for DRS rules
-data "vsphere_host" "cluster_hosts" {
-  count         = 3
-  name          = "esxi-host-${count.index + 1}"
-  datacenter_id = var.datacenter_id
+# Create vSphere tags for resource management
+resource "vsphere_tag_category" "tanzu_environment" {
+  name               = "tanzu-environment"
+  description        = "Tanzu environment classification"
+  cardinality        = "SINGLE"
+  
+  associable_types = [
+    "VirtualMachine",
+    "ResourcePool",
+    "Folder"
+  ]
 }
 
-# Create DRS anti-affinity rules for control plane nodes
-resource "vsphere_compute_cluster_vm_anti_affinity_rule" "control_plane_anti_affinity" {
-  name                = "control-plane-anti-affinity"
-  compute_cluster_id  = var.cluster_id
-  virtual_machine_ids = [] # This will be populated by the cluster modules
-  enabled             = true
-  mandatory           = true
+resource "vsphere_tag" "environment_tag" {
+  name        = var.environment
+  category_id = vsphere_tag_category.tanzu_environment.id
+  description = "Environment tag for ${var.environment}"
 }
 
-# Create DRS anti-affinity rules for worker nodes
-resource "vsphere_compute_cluster_vm_anti_affinity_rule" "worker_anti_affinity" {
-  name                = "worker-anti-affinity"
-  compute_cluster_id  = var.cluster_id
-  virtual_machine_ids = [] # This will be populated by the cluster modules
-  enabled             = true
-  mandatory           = false
+resource "vsphere_tag_category" "tanzu_cluster_role" {
+  name               = "tanzu-cluster-role"
+  description        = "Tanzu cluster role classification"
+  cardinality        = "SINGLE"
+  
+  associable_types = [
+    "VirtualMachine"
+  ]
+}
+
+resource "vsphere_tag" "control_plane_tag" {
+  name        = "control-plane"
+  category_id = vsphere_tag_category.tanzu_cluster_role.id
+  description = "Control plane node tag"
+}
+
+resource "vsphere_tag" "worker_tag" {
+  name        = "worker"
+  category_id = vsphere_tag_category.tanzu_cluster_role.id
+  description = "Worker node tag"
+}
+
+# Create VM storage policies if needed
+resource "vsphere_storage_drs_vm_override" "tanzu_storage_drs" {
+  count               = var.enable_storage_drs ? 1 : 0
+  datastore_cluster_id = var.datastore_cluster_id
+  virtual_machine_id   = data.vsphere_virtual_machine.kubernetes_template[0].id
+  sdrs_enabled         = true
+  sdrs_automation_level = "fullyAutomated"
+  sdrs_intra_vm_affinity = true
+}
+
+# Output important resource IDs and information
+output "folder_id" {
+  description = "VM folder ID for Tanzu resources"
+  value       = vsphere_folder.tanzu_folder.id
+}
+
+output "resource_pool_id" {
+  description = "Resource pool ID for Tanzu workloads"
+  value       = vsphere_resource_pool.tanzu_resource_pool.id
+}
+
+output "custom_attributes" {
+  description = "Custom attributes for Tanzu resources"
+  value = {
+    cluster_name = vsphere_custom_attribute.tanzu_cluster_name.id
+    cluster_role = vsphere_custom_attribute.tanzu_cluster_role.id
+    environment  = vsphere_custom_attribute.tanzu_environment.id
+  }
+}
+
+output "tags" {
+  description = "vSphere tags for resource management"
+  value = {
+    environment_category = vsphere_tag_category.tanzu_environment.id
+    environment_tag      = vsphere_tag.environment_tag.id
+    role_category        = vsphere_tag_category.tanzu_cluster_role.id
+    control_plane_tag    = vsphere_tag.control_plane_tag.id
+    worker_tag          = vsphere_tag.worker_tag.id
+  }
+}
+
+output "template_info" {
+  description = "VM template information"
+  value = var.create_templates ? {
+    kubernetes_template_id = data.vsphere_virtual_machine.kubernetes_template[0].id
+    kubernetes_template_name = data.vsphere_virtual_machine.kubernetes_template[0].name
+  } : {}
 }
