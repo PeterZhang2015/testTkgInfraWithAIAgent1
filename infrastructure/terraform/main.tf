5cbb55aa0c05a13d0eac5ff0a2bea035e9b26f1a
CHONGZHENG ZHANG <zhangchongzheng@gmail.com>
2025-07-09 22:06:11 +1000

    Add main Terraform configuration for vSphere Tanzu infrastructure

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,29 +1,29 @@
 # VMware vSphere Tanzu Kubernetes Infrastructure
-# Main Terraform configuration for deploying HA Tanzu infrastructure
+# Main Terraform configuration for deploying HA Tanzu Kubernetes Grid
 
 terraform {
   required_version = ">= 1.0"
   required_providers {
     vsphere = {
       source  = "hashicorp/vsphere"
-      version = "~> 2.4"
+      version = "~> 2.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.16"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.7"
     }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.1"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
   }
 }
 
-# Configure the VMware vSphere Provider
+# Configure vSphere provider
 provider "vsphere" {
   user                 = var.vsphere_username
   password             = var.vsphere_password
@@ -31,148 +31,121 @@ provider "vsphere" {
   allow_unverified_ssl = var.allow_unverified_ssl
 }
 
-# Data sources for existing vSphere resources
-data "vsphere_datacenter" "dc" {
+# Data sources for vSphere resources
+data "vsphere_datacenter" "datacenter" {
   name = var.vsphere_datacenter
 }
 
 data "vsphere_datastore" "datastore" {
   name          = var.vsphere_datastore
-  datacenter_id = data.vsphere_datacenter.dc.id
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-data "vsphere_resource_pool" "pool" {
-  name          = var.vsphere_resource_pool
-  datacenter_id = data.vsphere_datacenter.dc.id
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere_cluster
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_network" "network" {
   name          = var.vsphere_network
-  datacenter_id = data.vsphere_datacenter.dc.id
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_virtual_machine" "template" {
   name          = var.vm_template
-  datacenter_id = data.vsphere_datacenter.dc.id
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-# Local values for common configurations
-locals {
-  common_tags = {
-    Environment = var.environment
-    Project     = "tanzu-k8s-infrastructure"
-    Owner       = var.owner
-    CreatedBy   = "terraform"
-  }
-}
-
-# Resource groups and folders
-resource "vsphere_folder" "vm_folder" {
-  path          = var.vm_folder
-  type          = "vm"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-# Network configuration
-module "networking" {
-  source = "./modules/networking"
-  
-  datacenter_id = data.vsphere_datacenter.dc.id
-  environment   = var.environment
-  common_tags   = local.common_tags
-}
-
-# Storage configuration
-module "storage" {
-  source = "./modules/storage"
-  
-  datacenter_id = data.vsphere_datacenter.dc.id
-  environment   = var.environment
-  common_tags   = local.common_tags
-}
-
-# Security configuration
-module "security" {
-  source = "./modules/security"
-  
-  datacenter_id = data.vsphere_datacenter.dc.id
-  environment   = var.environment
-  common_tags   = local.common_tags
-}
-
-# vSphere base infrastructure
+# Module for vSphere base infrastructure
 module "vsphere_base" {
   source = "./modules/vsphere-base"
+
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+  datastore_id  = data.vsphere_datastore.datastore.id
+  cluster_id    = data.vsphere_compute_cluster.cluster.id
+  network_id    = data.vsphere_network.network.id
+  template_id   = data.vsphere_virtual_machine.template.id
+
+  resource_pool_name = var.resource_pool_name
+  vm_folder_name     = var.vm_folder_name
   
-  datacenter_id      = data.vsphere_datacenter.dc.id
-  datastore_id       = data.vsphere_datastore.datastore.id
-  resource_pool_id   = data.vsphere_resource_pool.pool.id
-  network_id         = data.vsphere_network.network.id
-  template_id        = data.vsphere_virtual_machine.template.id
-  vm_folder_id       = vsphere_folder.vm_folder.id
-  environment        = var.environment
-  common_tags        = local.common_tags
+  common_tags = var.common_tags
+}
+
+# Module for networking configuration
+module "networking" {
+  source = "./modules/networking"
+
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+  cluster_id    = data.vsphere_compute_cluster.cluster.id
+  
+  network_config = var.network_config
+  common_tags    = var.common_tags
+}
+
+# Module for storage configuration
+module "storage" {
+  source = "./modules/storage"
+
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+  cluster_id    = data.vsphere_compute_cluster.cluster.id
+  
+  storage_config = var.storage_config
+  common_tags    = var.common_tags
+}
+
+# Module for security configuration
+module "security" {
+  source = "./modules/security"
+
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+  cluster_id    = data.vsphere_compute_cluster.cluster.id
+  
+  security_config = var.security_config
+  common_tags     = var.common_tags
+}
+
+# Local values for common configuration
+locals {
+  common_vm_config = {
+    datacenter_id = data.vsphere_datacenter.datacenter.id
+    datastore_id  = data.vsphere_datastore.datastore.id
+    cluster_id    = data.vsphere_compute_cluster.cluster.id
+    network_id    = data.vsphere_network.network.id
+    template_id   = data.vsphere_virtual_machine.template.id
+    
+    resource_pool_id = module.vsphere_base.resource_pool_id
+    vm_folder_id     = module.vsphere_base.vm_folder_id
+  }
 }
 
 # Management cluster environment
 module "management_cluster" {
   source = "./environments/mgmt"
-  
-  datacenter_id      = data.vsphere_datacenter.dc.id
-  datastore_id       = data.vsphere_datastore.datastore.id
-  resource_pool_id   = data.vsphere_resource_pool.pool.id
-  network_id         = data.vsphere_network.network.id
-  template_id        = data.vsphere_virtual_machine.template.id
-  vm_folder_id       = vsphere_folder.vm_folder.id
-  environment        = "management"
-  common_tags        = local.common_tags
+
+  vm_config   = local.common_vm_config
+  cluster_config = var.management_cluster_config
+  common_tags = var.common_tags
 }
 
 # Development cluster environment
 module "dev_cluster" {
   source = "./environments/dev"
+
+  vm_config      = local.common_vm_config
+  cluster_config = var.dev_cluster_config
+  common_tags    = var.common_tags
   
-  datacenter_id      = data.vsphere_datacenter.dc.id
-  datastore_id       = data.vsphere_datastore.datastore.id
-  resource_pool_id   = data.vsphere_resource_pool.pool.id
-  network_id         = data.vsphere_network.network.id
-  template_id        = data.vsphere_virtual_machine.template.id
-  vm_folder_id       = vsphere_folder.vm_folder.id
-  environment        = "development"
-  common_tags        = local.common_tags
+  depends_on = [module.management_cluster]
 }
 
 # Production cluster environment
 module "prod_cluster" {
   source = "./environments/prod"
+
+  vm_config      = local.common_vm_config
+  cluster_config = var.prod_cluster_config
+  common_tags    = var.common_tags
   
-  datacenter_id      = data.vsphere_datacenter.dc.id
-  datastore_id       = data.vsphere_datastore.datastore.id
-  resource_pool_id   = data.vsphere_resource_pool.pool.id
-  network_id         = data.vsphere_network.network.id
-  template_id        = data.vsphere_virtual_machine.template.id
-  vm_folder_id       = vsphere_folder.vm_folder.id
-  environment        = "production"
-  common_tags        = local.common_tags
-}
-
-# Outputs
-output "management_cluster_info" {
-  description = "Management cluster information"
-  value       = module.management_cluster.cluster_info
-}
-
-output "dev_cluster_info" {
-  description = "Development cluster information"
-  value       = module.dev_cluster.cluster_info
-}
-
-output "prod_cluster_info" {
-  description = "Production cluster information"
-  value       = module.prod_cluster.cluster_info
-}
-
-output "vsphere_base_info" {
-  description = "vSphere base infrastructure information"
-  value       = module.vsphere_base.infrastructure_info
+  depends_on = [module.management_cluster]
 }
